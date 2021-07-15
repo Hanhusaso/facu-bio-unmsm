4d257efea5ed3517024a5d5c2e4ea318fb6eac44
Hanhusaso <hans.e.huiza.n@gmail.com>
2021-07-15 16:31:16 -0500

    video de fondo del home actualizado

--- pages/index.js
+++ pages/index.js
@@ -36,7 +36,7 @@ export default function Home() {
         <main>
           <section className="section-principal-home section-eventos">
             <video className="bg-home-video" loop autoplay="true" muted>
-              <source src="/assets/video/flower.mp4" type="video/mp4"/>
+              <source src="/assets/video/Prueba1_1.mp4" type="video/mp4"/>
             </video>
             <div className="wrapper-title-home">
               <Container fluid>
