b1171197439c7e528ebd945c9b22f059310c69b6
Hanhusaso <hans.e.huiza.n@gmail.com>
2021-07-16 14:38:37 -0500

    loader page y componente de noticia

--- pages/comunidad/cerseu/index.js
+++ pages/comunidad/cerseu/index.js
@@ -1,6 +1,7 @@
 import React , { useEffect } from 'react'
 import Layout from '../../../components/Layout'
 import {Row, Col, Container, Breadcrumb} from 'react-bootstrap'
+import NoticiasExtra from '../../../components/NoticiasExtra'
 import Link from 'next/link'
 
 import { useWindowSize } from '../../../utils/useWindowSize'
@@ -215,62 +216,7 @@ const index = () => {
                                                 </li>
                                             </ul>
                                         </div>
-                                        <div className="d-flex mb-3">
-                                            <h2 className="subtitle-green mr-1 mb-0">Noticias</h2>
-                                            <a href="/noticias" className="d-inline-block">
-                                                <img src="/assets/img/iconos/boton_vermas.png"/>
-                                            </a>
-                                        </div>
-                                        <div>
-                                            <div className="card-bio mb-3">
-                                                <div className="part-img position-relative">
-                                                    <div className="position-relative">
-                                                        <img className="w-100 img-fluid" src="/assets/img/noticias/noticia2.png"></img>
-                                                        <div className="dark-filter"></div>
-                                                    </div>
-                                                    <div className="badge-tag">
-                                                        Tecnologia
-                                                    </div>
-                                                </div>
-                                                <div className="part-text">
-                                                    <p className="title-card mb-0">
-                                                        Dos científicas ganan el Premio Nobel de Química 2020 por “tijeras genéticas” CRISPR-Cas9
-                                                    </p>
-                                                </div>
-                                            </div>
-                                            <div className="card-bio mb-3">
-                                                <div className="part-img position-relative">
-                                                    <div className="position-relative">
-                                                        <img className="w-100 img-fluid" src="/assets/img/noticias/noticia3.png"></img>
-                                                        <div className="dark-filter"></div>
-                                                    </div>
-                                                    <div className="badge-tag">
-                                                        Ciencia
-                                                    </div>
-                                                </div>
-                                                <div className="part-text">
-                                                    <p className="title-card mb-0">
-                                                        El objetivo de nuestro proyecto es ir en camino a producir clones de alpacas
-                                                    </p>
-                                                </div>
-                                            </div>
-                                            <div className="card-bio mb-3">
-                                                <div className="part-img position-relative">
-                                                    <div className="position-relative">
-                                                        <img className="w-100 img-fluid" src="/assets/img/noticias/noticia4.png"></img>
-                                                        <div className="dark-filter"></div>
-                                                    </div>
-                                                    <div className="badge-tag">
-                                                        Facultad
-                                                    </div>
-                                                </div>
-                                                <div className="part-text">
-                                                    <p className="title-card mb-0">
-                                                        ¡La FCB ya cuenta oficialmente con un nuevo y moderno pabellón!
-                                                    </p>
-                                                </div>
-                                            </div>
-                                        </div>
+                                        <NoticiasExtra/>
                                     </aside>
                                 </Col>
                                 <Col md="1" lg="1"></Col>
