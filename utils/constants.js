201f41d79d6e7938e9f103a4f5df8fd5114855ed
Andy Yupanqui <andyyupanqui@gmail.com>
2021-07-20 22:33:32 -0500

    imagenes s3

--- utils/constants.js
+++ utils/constants.js
@@ -1,2 +1,2 @@
 // export const BASE_PATH = "http://localhost:1337";
-export const BASE_PATH = "http://18.116.174.43:1337";
+export const BASE_PATH = process.env.NODE_ENV==="development"?"http://localhost:1337":"http://118.116.174.43:1337";
