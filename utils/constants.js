e0131daecf13349de741496f15d35037724c6a89
Hanhusaso <hans.e.huiza.n@gmail.com>
2021-07-22 18:33:33 -0500

    corrección de IP de back

--- utils/constants.js
+++ utils/constants.js
@@ -1,3 +1,3 @@
 // export const BASE_PATH = "http://localhost:1337";
 export const BASE_PATH =
-	process.env.NODE_ENV === "development" ? "http://localhost:1337" : "https://biologiaadmin.unmsm.edu.pe/";
+	process.env.NODE_ENV === "development" ? "http://localhost:1337" : "https://biologiaadmin.unmsm.edu.pe";
