036e85b6b139db235b8cacc8c7d7fe04ff6db197
Andy Yupanqui <andyyupanqui@gmail.com>
2021-07-26 17:20:24 -0500

    agregando orden por prioridad en seccion directorios

--- pages/api/directorio.js
+++ pages/api/directorio.js
@@ -5,11 +5,11 @@ export async function getDirectoriosApi(palabra){
         var query = '';
 
         if(palabra != ""){
-            query = query + `/?_where[_or][0][unidad_contains]=${palabra}&_where[_or][1][nombre_contains]=${palabra}`
+            query = query + `&_where[_or][0][unidad_contains]=${palabra}&_where[_or][1][nombre_contains]=${palabra}`
             // query = query + `/?nombre_contains=${palabra}`;
         }
 
-        const url = `${BASE_PATH}/directorios${query}`;
+        const url = `${BASE_PATH}/directorios/?_sort=orden_prioridad:ASC${query}`;
         const response = await fetch(url);
         const result = await response.json();
         return result;
