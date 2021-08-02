a88bd29256677f07c038bd077759b4d966b0d1ff
Hanhusaso <hans.e.huiza.n@gmail.com>
2021-08-02 16:51:26 -0500

    estilos del modal funcionan

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 // import '../styles/globals.css'
 import '../sass/app.scss'
 // import 'semantic-ui-css/semantic.min.css';
+import '../styles/uimodal.min.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
