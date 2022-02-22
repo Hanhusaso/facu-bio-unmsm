c395e005a60da95193d77f51f6983e7546dca6fa
Luis Castillo <lcastillov123@gmail.com>
2022-02-22 12:29:30 -0500

    Actualización en páginas de posgrado. Nuevo modelo de back y eliminación de enlaces de investigación

--- pages/api/formacion-academica.js
+++ pages/api/formacion-academica.js
@@ -1,13 +1,14 @@
 import { BASE_PATH } from "../../utils/constants";
 
-export async function getDocenteByIdInformacionAcademicaApi(formacion_academica){
-    try {
-        const url = `${BASE_PATH}/docentes/?formacion_academica=${formacion_academica}`;
-        const response = await fetch(url);
-        const result = await response.json();
-        return result;
-    } catch (error) {
-        console.log(error);
-        return [];
-    }
+export async function getDocenteByIdInformacionAcademicaApi(formacion_academica) {
+	try {
+		// const url = `${BASE_PATH}/docentes/?formacion_academica=${formacion_academica}`;
+		const url = `${BASE_PATH}/docentes/?formacion_pogrados.nombre_in=${formacion_academica}`;
+		const response = await fetch(url);
+		const result = await response.json();
+		return result;
+	} catch (error) {
+		console.log(error);
+		return [];
+	}
 }
