82cb091803b32aeff5dd45001dc9413508eb25a5
Luis <luisivan.castillovillafuerte@externos-cl.cencosud.com>
2024-01-26 15:52:12 -0500

    feat: docentes

--- pages/api/formacion-academica.js
+++ pages/api/formacion-academica.js
@@ -1,9 +1,11 @@
-import { BASE_PATH } from "../../utils/constants";
+import { BASE_PATH } from '../../utils/constants';
 
-export async function getDocenteByIdInformacionAcademicaApi(formacion_academica) {
+export async function getDocenteByIdInformacionAcademicaApi(
+	formacion_academica
+) {
 	try {
 		// const url = `${BASE_PATH}/docentes/?formacion_academica=${formacion_academica}`;
-		const url = `${BASE_PATH}/docentes/?formacion_pogrados.nombre_in=${formacion_academica}`;
+		const url = `${BASE_PATH}/docentes/?formacion_pogrados.nombre_in=${formacion_academica}&_sort=nombre:ASC`;
 		const response = await fetch(url);
 		const result = await response.json();
 		return result;
